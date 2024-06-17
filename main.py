dbe4447a22ae35004f1c02d0f121cd7d6eb44dd8
George Hannah <georgehannah@hotmail.co.uk>
2024-06-17 13:41:23 +0100

    trying to fix JSON reading

---
+++ main.py
@@ -0,0 +1,45 @@
+# General imports
+import argparse
+import json
+import importlib
+
+# Module imports
+# import elementExtract
+# import processComplexLabel
+
+
+def run(args):
+
+    # Build pipeline from module list
+    pipeline: []
+    moduleFile = "home/george/Documents/GitHub/X2O/pipeline.json"
+    with open(moduleFile, "r") as m:
+        modules = json.load(m)
+    for i in modules:
+        print(i)
+
+    schema = args.schema
+
+
+# Handle and bind arguments
+def main():
+    parser = argparse.ArgumentParser(
+        prog="X2O",
+        description="A modular XSD to ontology translation pipeline"
+    )
+    parser.add_argument(
+        "-s",
+        "--schema",
+        type=str,
+        help="The filename of the input XSD file",
+        dest="schema",
+        required=True
+    )
+
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
