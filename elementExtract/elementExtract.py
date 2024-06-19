7f353d3e448f18ffe80d130f752b3b59d884ed53
George Hannah <georgehannah@hotmail.co.uk>
2024-06-19 13:16:11 +0100

    Add automatic prefix identification and binding

--- elementExtract/elementExtract.py
+++ elementExtract/elementExtract.py
@@ -1,5 +1,6 @@
 import xml.dom.minidom as dom
 
+
 # Generate prefix to namespace bindings
 def resolveNameSpace(element, prefix):
     ns = None
@@ -11,26 +12,35 @@ def resolveNameSpace(element, prefix):
         for child in element.childNodes:
             try:
                 ns = resolveNameSpace(child, prefix)
-            except:
+            except Exception:
                 pass
             if ns is not None:
                 return ns
     return ns
 
-# TODO: Scan through the file to identify all prefixes used (so we can then look to generate bindings for them) (This will improve interoperability with other schemata
-def findPrefixes():
-    pass
+
+# Identify all prefixes used within the schema
+def findPrefixes(root):
+    prefixes = []
+    try:
+        tagName = root.tagName
+        prefix = tagName.split(":")[0]
+        prefixes.append(prefix)
+    except Exception:
+        pass
+    for child in root.childNodes:
+        childPrefixes = findPrefixes(child)
+        for p in childPrefixes:
+            prefixes.append(p)
+    return prefixes
+
 
 def elementExtract(schemaPath):
     schema = dom.parse(schemaPath)
 
     root = schema.documentElement
 
-    # TODO: Look into how namespaces are handled as abreviation can be different
-    prefixesStr = input("Enter all prefixes used in the schema seperated by commas: ")
-    prefixesStr = prefixesStr.replace(" ", "")
-    print(prefixesStr)
-    prefixes = prefixesStr.split(",")
+    prefixes = findPrefixes(root)
     nsURIs = {prefix: resolveNameSpace(root, prefix) for prefix in prefixes}
 
     for prefix, ns in nsURIs.items():
