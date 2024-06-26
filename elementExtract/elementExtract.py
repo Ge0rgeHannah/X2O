dfc6f8d4473d22012ba70f334a79ff0e2cab6794
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2024-06-26 10:26:07 +0100

    Identify the different XML schema elements

--- elementExtract/elementExtract.py
+++ elementExtract/elementExtract.py
@@ -35,20 +35,100 @@ def findPrefixes(root):
     return prefixes
 
 
+def getComplexTypes(schema, xmlsPrefix):
+    ComplexTypesBasic = schema.getElementsByTagName(
+        xmlsPrefix + ":complexType")
+    ComplexTypes = []
+    seenComplexTypes = set()
+
+    for e in ComplexTypesBasic:
+        if e.hasAttribute("name"):
+            name = e.getAttribute("name")
+            if name not in seenComplexTypes:
+                seenComplexTypes.add(name)
+                ComplexTypes.append(e)
+
+    print("-- complexType elements --")
+    for i in ComplexTypes:
+        print(i.getAttribute("name"))
+
+
+def getSimpleTypes(schema, xmlsPrefix):
+    SimpleTypesBasic = schema.getElementsByTagName(xmlsPrefix + ":simpleType")
+    SimpleTypes = []
+    seenSimpleTypes = set()
+
+    for e in SimpleTypesBasic:
+        if e.hasAttribute("name"):
+            name = e.getAttribute("name")
+            if name not in seenSimpleTypes:
+                seenSimpleTypes.add(name)
+                SimpleTypes.append(e)
+
+    print("-- simpleType elements --")
+    for i in SimpleTypes:
+        print(i.getAttribute("name"))
+
+
+def getAttributes(schema, xmlsPrefix):
+    AttributesBasic = schema.getElementsByTagName(xmlsPrefix + ":attribute")
+    Attributes = []
+    seenAttributes = set()
+
+    for e in AttributesBasic:
+        if e.hasAttribute("name"):
+            name = e.getAttribute("name")
+            if name not in seenAttributes:
+                seenAttributes.add(name)
+                Attributes.append(e)
+
+    print("-- attributes --")
+    for i in Attributes:
+        print(i.getAttribute("name"))
+
+
+def getAttributeGroups(schema, xmlsPrefix):
+    AttributeGroupsBasic = schema.getElementsByTagName(
+        xmlsPrefix + ":attributeGroup")
+    AttributeGroups = []
+    seenAttributeGroups = set()
+
+    for e in AttributeGroupsBasic:
+        if e.hasAttribute("name"):
+            name = e.getAttribute("name")
+            if name not in seenAttributeGroups:
+                seenAttributeGroups.add(name)
+                AttributeGroups.append(e)
+
+    print("-- attributeGroups --")
+    for i in AttributeGroups:
+        print(i.getAttribute("name"))
+
+
 def elementExtract(schemaPath):
     schema = dom.parse(schemaPath)
 
     root = schema.documentElement
 
     prefixes = findPrefixes(root)
-    nsURIs = {prefix: resolveNameSpace(root, prefix) for prefix in prefixes}
+    nsURIs = {resolveNameSpace(root, prefix): prefix for prefix in prefixes}
 
-    for prefix, ns in nsURIs.items():
-        print(f"Namespace URI for prefix '{prefix}': {ns}")
+    # for prefix, ns in nsURIs.items():
+    #    print(f"Namespace is bound to: '{prefix}': {ns}")
 
-    # ComplexTypes = schema.getElementsByTagName("xsd:complexType")
-    # for i in ComplexTypes:
-    #    print(i.getAttribute("name"))
+    xmlsPrefix = nsURIs["http://www.w3.org/2001/XMLSchema"]
+
+    # Identify complexType elements
+    getComplexTypes(schema, xmlsPrefix)
+
+    # Identify simpleType elements
+    getSimpleTypes(schema, xmlsPrefix)
+
+    # Identify attribute elements
+    getAttributes(schema, xmlsPrefix)
+
+    # Identify attributeGroup elements
+    getAttributeGroups(schema, xmlsPrefix)
 
 
 if __name__ == "__main__":
